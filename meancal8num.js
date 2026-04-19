7cc72732e544c571d0e081483b6e884740433583
Teeandnarita <128231239+Teeandnarita@users.noreply.github.com>
2026-04-19 09:57:30 +0700

    Create meancal8num.js

---
+++ meancal8num.js
@@ -0,0 +1,10 @@
+numbers = [2,4,4,5,8,10,12,15]
+total = 0
+
+for (let i = 0;i < numbers.length; i++){
+    total += numbers[i]
+}
+    console.log(total)
+    
+    finaltotal = total/numbers.length
+    console.log(finaltotal)
